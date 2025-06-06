db7d455785f2e0f48c79ad79bca36fcd0b8a952d
Besik Gugushvili <gugushvili.beso0013@gmail.com>
2025-06-06 19:48:32 +0400

    chore: migrate to vue2

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <h1 class="text-2xl font-bold">Block Mail Studio - Vue 2</h1>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style scoped>
+</style>
